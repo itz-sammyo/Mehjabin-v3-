2a977d11499a2b018021560a2734aa84f1ce7996
itz-sammyo <itzxsammyo33@gmail.com>
2026-02-27 21:11:24 +0600

    Update aniqz2.js

--- scripts/cmds/aniqz2.js
+++ scripts/cmds/aniqz2.js
@@ -60,7 +60,7 @@ module.exports = {
       }, event.messageID);
     } catch (error) {
       console.error(error);
-      api.sendMessage("🥹error, contact MahMUD.", event.threadID, event.messageID);
+      api.sendMessage("🥹error, contact It'z Sammyo.", event.threadID, event.messageID);
     }
   },
 
